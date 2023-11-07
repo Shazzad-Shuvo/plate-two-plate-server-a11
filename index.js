9100f74b27f5f77c931849de5590257f0d6b3f68
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2023-11-08 02:03:07 +0600

    install cookie parser, import jwt and cookie parser and use cookie parser in middleware

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -14,6 +16,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser());
 
 
 

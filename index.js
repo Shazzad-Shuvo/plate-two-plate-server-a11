35ac94484727bb18e30ddaaa0d6888abe719884c
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2023-11-09 13:46:00 +0600

    food request delete api created

--- index.js
+++ index.js
@@ -128,6 +128,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/foodRequests/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await foodRequestCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
 
